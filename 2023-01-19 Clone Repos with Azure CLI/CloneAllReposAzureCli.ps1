8cdc85f0591caec64bdcba8d214756b64cc1c901
Arne Moerman <arne.moerman@sioen.com>
2023-01-31 17:16:27 +0100

    Add project files.

---
+++ 2023-01-19 Clone Repos with Azure CLI/CloneAllReposAzureCli.ps1
@@ -0,0 +1,29 @@
+
+$initpath = get-location
+$json_projects = Invoke-Expression "az devops project list --organization https://dev.azure.com/arnemoermansioen/ --output json --only-show-errors" | Convertfrom-json
+
+foreach ($project in $json_projects.value){
+    if($project.name -like ' dont use TFS - *'){ continue }
+
+    $project_name = $project.name
+    Write-Host "-- Project: "$project_name" --"
+    
+    $repos_in_project = Invoke-Expression "az repos list --organization https://dev.azure.com/arnemoermansioen/ --project ""$project_name"" --output json" | Convertfrom-json
+    foreach ($repo in $repos_in_project){
+        
+        $name = $project_name + "\" + $repo.name
+
+        if (!(Test-Path -Path $name)) {
+            Write-Host "- Cloning: "$repo.name
+            git clone $repo.remoteUrl $name
+            Write-Host "- Done Cloning: "$repo.name
+        }
+        else {
+            Write-Host "- Pulling: "$repo.name
+            set-location $name
+            git pull
+            set-location $initpath
+            Write-Host "- Done Pulling: "$repo.name
+        }    
+    }
+}
---
+++ 2023-01-27 Remove Required Reviewers from policy with Azure CLI/RemoveRequiredReviewersPolicy.ps1
@@ -0,0 +1,26 @@
+﻿
+$json_projects = Invoke-Expression "az devops project list --organization https://dev.azure.com/sioen --output json --only-show-errors" | Convertfrom-json
+
+foreach ($project in $json_projects.value) {
+    if ($project.name -like ' dont use TFS - *') { continue }
+
+    Write-Host "-- Project: "$($project.name)" --"
+    
+    $json_repos = Invoke-Expression "az repos list --organization https://dev.azure.com/sioen --project ""$($project.name)"" --output json" | Convertfrom-json
+    #Write-Host "ran: az repos list --organization https://dev.azure.com/sioen --project ""$($project.name)"" --output json"
+
+    foreach ($repo in $json_repos) {        
+        Write-Host "- Project & Repo: $($project.name) \ $($repo.name) -"
+        
+        $json_policies = Invoke-Expression "az repos policy list --organization https://dev.azure.com/sioen --project ""$($project.name)"" --repository-id ""$($repo.id)"" --branch main --output json" | Convertfrom-json                
+        #Write-Host "ran: az repos policy list --organization https://dev.azure.com/sioen --project ""$($project.name)"" --repository-id ""$($repo.id)"" --branch main --output json"
+        
+        foreach ($policy in $json_policies) {
+            if ($($policy.type.displayName) -eq "Required reviewers") {
+
+                Write-Host "running: az repos policy delete --id $($policy.id)  --organization https://dev.azure.com/sioen --project ""$($project.name)"" --output json" -ForegroundColor Yellow
+                az repos policy delete --id $($policy.id)  --organization https://dev.azure.com/sioen --project ""$($project.name)"" --output json --yes
+            }
+        }        
+    }
+}
